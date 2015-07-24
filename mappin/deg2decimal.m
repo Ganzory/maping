ff50a7b1de5258261d9dd75cf4b6e2d07fa89bd7
Ganzory <eng.ganzory.comm@gmail.com>
2015-07-24 16:12:47 +0200

    a5er 7aga liza
    sho8l

--- mappin/deg2decimal.m
+++ mappin/deg2decimal.m
@@ -1,4 +1,4 @@
-function decci = deg2decimal(bef,aft)
-        
-        decci =  floor(bef/100) +((((bef/100)-floor(bef/100))*100)+aft/1e+5)/60;
+function decci = deg2decimal(bef,aft,varargin)
+        min =varargin{1}/60; 
+        decci =  floor(bef/100) +(((((bef/100)-floor(bef/100))*100)+aft/1e+5)+min)/60;
 end
